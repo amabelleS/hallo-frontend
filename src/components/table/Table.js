d61b7b2541b89d34d50f2c4f13936a7601c0b824
amabelle <amabelletr@gmail.com>
2022-02-10 23:09:41 +0200

    Fetching fetchMaxSalaryByJob

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { useFetchApiForTable } from '../../hooks/useFetchApiForTable';
 
 const Table = () => {
-  const { avgSalary } = useFetchApiForTable();
+  const { maxSalaryByJob, avgSalary } = useFetchApiForTable();
+  console.log(
+    '🚀 ~ file: table.js ~ line 6 ~ Table ~ maxSalaryByJob',
+    maxSalaryByJob
+  );
   console.log('🚀 ~ file: table.js ~ line 6 ~ Table ~ avgSalary', avgSalary);
   return <div>table</div>;
 };
--- src/hooks/useFetchApiForTable.js
+++ src/hooks/useFetchApiForTable.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 
 export const useFetchApiForTable = () => {
-  const [maxSalaryByJob, setMaxSalaryByJob] = useState({});
+  const [maxSalaryByJob, setMaxSalaryByJob] = useState([]);
   const [avgSalary, setAvgSalary] = useState([]);
+  const [jobs, setJobes] = useState([]);
 
   const baseURL = 'http://localhost:8080/salary/';
 
@@ -10,22 +11,46 @@ export const useFetchApiForTable = () => {
     try {
       const response = await fetch(`${baseURL}avg`);
       const data = await response.json();
+      const fetchedJobs = data.map((job) => job.job);
+      setJobes(fetchedJobs);
       console.log(
-        '🚀 ~ file: useFetchApiForTable.js ~ line 12 ~ fetchAvgSalary ~ data',
-        data
+        '🚀 ~ file: useFetchApiForTable.js ~ line 20 ~ fetchAvgSalary ~ fetchedJobs',
+        fetchedJobs
       );
       return data;
     } catch (error) {}
   };
 
   const fetchMaxSalaryByJob = async () => {
-    const allJobs = [];
-    const response = await fetch(`${baseURL}`);
+    try {
+      const maxJobs = await Promise.all(
+        jobs.map(async (job) => {
+          const fetchedJobs = await fetch(`${baseURL}${job}`);
+          return fetchedJobs.json();
+        })
+      );
+      console.log(
+        '🚀 ~ file: useFetchApiForTable.js ~ line 32 ~ fetchMaxSalaryByJob ~ maxJobs',
+        maxJobs
+      );
+
+      //   const fetchedMaxJobs = maxJobs
+      //     .filter((p) => p.status === 'fulfilled')
+      //     .map((job) => job.value.data.results[0]);
+
+      return maxJobs;
+    } catch (error) {
+      throw Error(error);
+    }
   };
 
   useEffect(() => {
     fetchAvgSalary().then((salaries) => setAvgSalary(salaries));
   }, []);
+
+  useEffect(() => {
+    fetchMaxSalaryByJob().then((maxJobs) => setMaxSalaryByJob(maxJobs));
+  }, [avgSalary]);
 
   return { maxSalaryByJob, avgSalary };
 };
