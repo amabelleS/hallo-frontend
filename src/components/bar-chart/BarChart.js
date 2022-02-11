efa5c1159f48b8ab558d2d9824a4c9f0b96ee4f9
amabelle <amabelletr@gmail.com>
2022-02-11 03:18:41 +0200

    Displaying a bar-chart

--- src/components/bar-chart/BarChart.js
+++ src/components/bar-chart/BarChart.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import { useFetchPopularity } from '../../hooks/useFetchPopularity';
+import { __COLORS__ } from './colors';
+
+import './BarChart.css';
 
 const BarChart = () => {
   const jobsPopularity = useFetchPopularity();
+
+  const jobsToChart = jobsPopularity.map((job, i) => {
+    let popularityLogScale = Math.log10(job.popularity);
+    return {
+      name: job.job,
+      popularity: job.popularity,
+      popularityLogScale,
+      colors: __COLORS__[i],
+    };
+  });
   console.log(
-    '🚀 ~ file: BarChart.js ~ line 6 ~ BarChart ~ jobsPopularity',
-    jobsPopularity
+    '🚀 ~ file: BarChart.js ~ line 19 ~ jobsToChart ~ jobsToChart',
+    jobsToChart
   );
-  return <div>BarChart</div>;
+  return (
+    <div className="container">
+      <div className="main">
+        {jobsToChart.length > 0 &&
+          jobsToChart.map((job, i) => {
+            return (
+              <div key={i} className="bar-chart">
+                <div className="number" style={{ color: job.colors[1] }}>
+                  {job.popularity}
+                </div>
+                <div
+                  className="bar"
+                  style={{
+                    height: job.popularity * 4.2,
+                    // height: job.popularityLogScale * 111,
+                    backgroundImage: `linear-gradient(
+                        to bottom,
+                        ${job.colors[0]},
+                        ${job.colors[1]}
+                      )`,
+                  }}
+                ></div>
+                <div className="name">{job.name}</div>
+              </div>
+            );
+          })}
+      </div>
+      <div className="base"></div>
+    </div>
+  );
 };
 
 export default BarChart;
